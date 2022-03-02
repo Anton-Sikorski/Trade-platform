8801e9766a7a319431929b920848b272b424b141
Anton-Sikorski <Anton.Sikorski@itechart-group.com>
2022-03-02 19:22:42 +0300

    Remove logidze

--- config/application.rb
+++ config/application.rb
@@ -26,8 +26,6 @@ module TradePlatform
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    # logidze format
-    config.active_record.schema_format = :sql
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files

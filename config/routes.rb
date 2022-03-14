02e0fb9c288b92c0a329e4f97bf9a0f2a6e48d34
Anton-Sikorski <Anton.Sikorski@itechart-group.com>
2022-03-14 18:24:11 +0300

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  root 'category#index'
+  root 'categories#index'
 
-  resources :category do
-    resources :product
+  resources :categories do
+    resources :products
   end
 end

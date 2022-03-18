09e9710ae3953fd829e7c411826eada6dcd6c747
Anton-Sikorski <Anton.Sikorski@itechart-group.com>
2022-03-18 15:49:06 +0300

    Fix small issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'carts/show'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
-  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  devise_scope :user do
+    get '/users/sign_up', to: 'devise/registrations#new'
+    get '/users/sign_in', to: 'devise/sessions#new'
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
+  devise_for :users
+
   ActiveAdmin.routes(self)
 
-  root 'categories#index'
+  root to: 'categories#index'
 
+  get 'carts/show'
   resource :cart, only: [:show] do
     put 'add/:movie_id', to: 'carts#add', as: :add_to
     put 'remove/:movie_id', to: 'carts#remove', as: :remove_from

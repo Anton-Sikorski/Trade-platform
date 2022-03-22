48a02ac141cc4950da950d94c83cb4022fc8306a
Anton-Sikorski <Anton.Sikorski@itechart-group.com>
2022-03-22 11:01:02 +0300

    Fix rubocop issues. Fix rspec failrues

--- spec/controllers/carts_controller_spec.rb
+++ spec/controllers/carts_controller_spec.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 require "rails_helper"
+require_relative "../support/devise"
 
 RSpec.describe CartsController, type: :controller do
   describe "GET /show" do
+    let(:user) { create(:user) }
+
     it "returns http success" do
-      get "/carts/show"
+      sign_in user
+      get :show
+
       expect(response).to have_http_status(:success)
     end
   end

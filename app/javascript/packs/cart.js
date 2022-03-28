83946f3936e723cccc9fe3b3ac7bf5ad0fd99506
Anton-Sikorski <Anton.Sikorski@itechart-group.com>
2022-03-28 15:09:14 +0300

    Edit configs

--- app/javascript/packs/cart.js
+++ app/javascript/packs/cart.js
@@ -1,17 +1,16 @@
 $(document).ready(function() {
-    $('.removeBtn').click(function(){
-        $(this).parents('#cartProduct').hide()
 
+    $('.removeBtn').click(function(){
         let itemCount = $('#myCart .cart-count').text().match(/\d+/) - 1
+        $(this).parents('#cartProduct').hide()
         $('#myCart .cart-count').text( itemCount )
         $('header .cart-count').text( "(" + itemCount + ")" )
     });
 
     $('.add-to-cart').click(function (){
-        console.log('Click!')
-        $(this).parent().append($(
-            "<div>Added to cart!</div>"
-        ))
+        let itemCount = parseInt($('header .cart-count').text().match(/\d+/)) + 1
+        $('header .cart-count').text( "(" + itemCount + ")" )
+        $(this).parent().prepend($("<div>Added to cart!</div>"))
         $(this).hide()
     })
 })
